49a8e7f9c8e4c7a8848fee692c53f86e8868f6c9
chay0354 <chay.moalem@gmail.com>
2026-02-04 15:43:56 +0200

    Remove unused API_BASE_URL import from InfoTab

--- src/components/InfoTab.js
+++ src/components/InfoTab.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api, { API_BASE_URL } from '../utils/api';
+import api from '../utils/api';
 import './InfoTab.css';
 
 function InfoTab() {
